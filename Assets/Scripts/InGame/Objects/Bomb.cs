8e6653ba44dfdf027aab8de22e32c4715fb83c8e
martin-newbie <g.martin.min@gmail.com>
2023-03-17 17:19:34 +0900

    파티클

---
+++ Assets/Scripts/InGame/Objects/Bomb.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    ParticleSystem bombEffect;
+    CircleCollider2D hitCol;
+
+    void Start()
+    {
+        bombEffect = GetComponent<ParticleSystem>();
+        hitCol = GetComponent<CircleCollider2D>();
+
+        bombEffect.Play();
+        StartCoroutine(bombMovement(1f));
+    }
+
+    IEnumerator bombMovement(float duration)
+    {
+        var shape = bombEffect.shape;
+        float timer = 0f;
+
+        while (timer <= duration)
+        {
+            float sizeLerp = Mathf.Lerp(0.5f, 10f, timer / duration);
+
+            shape.radius = sizeLerp;
+            hitCol.radius = sizeLerp;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            var enemy = collision.GetComponent<EnemyBase>();
+            enemy.OnDamage(enemy.hp);
+        }
+    }
+}
